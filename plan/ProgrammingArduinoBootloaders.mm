0971fc4819fcc3c94bc6037a5e63792cd5b2dd24
Romilly Cocking <romilly.cocking@gmail.com>
2015-03-17 09:55:11 +0000

    Added license

---
+++ plan/ProgrammingArduinoBootloaders.mm
@@ -0,0 +1,33 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1426407818538" ID="ID_234016166" MODIFIED="1426408755479">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Programming
+    </p>
+    <p style="text-align: center">
+      Arduino
+    </p>
+    <p style="text-align: center">
+      Bootloaders
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1426407838787" ID="ID_389013401" MODIFIED="1426407843559" POSITION="right" TEXT="Raspberry Pi">
+<node CREATED="1426407864332" ID="ID_1600791555" MODIFIED="1426407871127" TEXT="G+ comment">
+<node CREATED="1426407873611" ID="ID_192904027" MODIFIED="1426407877983" TEXT="Project link"/>
+</node>
+<node CREATED="1426407851764" ID="ID_1847020142" MODIFIED="1426407858119" TEXT="Cuzner&apos;s code"/>
+<node CREATED="1426407883548" ID="ID_1792147995" MODIFIED="1426407912895" TEXT="Minor issues"/>
+<node CREATED="1426407893692" ID="ID_1804686329" MODIFIED="1426407897631" TEXT="What I did"/>
+<node CREATED="1426407931635" ID="ID_922932733" MODIFIED="1426407934055" TEXT="CTA">
+<node CREATED="1426407936099" ID="ID_1631938423" MODIFIED="1426407947471" TEXT="e-book signup"/>
+</node>
+</node>
+</node>
+</map>
